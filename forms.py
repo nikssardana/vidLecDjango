5c46b55363dab5874ecc66582b8225f909320fad
Nikhil Sardana <nikhilklk@gmail.com>
2015-12-09 00:25:00 +0530

    Fixed a bug, registration is successful now, password saved correctly now

--- forms.py
+++ forms.py
@@ -5,14 +5,14 @@ from django.contrib.auth.forms import UserCreationForm
 class MyRegistrationForm(UserCreationForm): 
     email = forms.EmailField(required=True)
 
-    class Meta: #fields that are to be shown in the registration form
+    class Meta:
         model = User
         fields = ('username','email','password1','password2')
 
-    def save(self,commit=True): #overriding the default save() method in django.contrib.auth.forms
-        user = super(UserCreationForm,self).save(commit=False)
+    def save(self,commit=True):
+        #user = super(UserCreationForm,self).save(commit=False)
+        user = super(MyRegistrationForm,self).save(commit=False)
         user.email = self.cleaned_data['email']
-
-        if commit: #commit variable is for consistency for now, may mean something ig we inherit this class
+        if commit:
             user.save()
         return user
