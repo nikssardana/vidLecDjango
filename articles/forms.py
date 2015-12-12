12f1ab5a1cfc4aa99e076756331867fc8a76b04a
Nikhil Sardana <nikhilklk@gmail.com>
2015-12-12 23:56:22 +0530

    Removed the likes field from the form, set its default value to zero

--- articles/forms.py
+++ articles/forms.py
@@ -4,4 +4,4 @@ from .models import Article
 class ArticleForm(forms.ModelForm):
     class Meta:
         model = Article
-        exclude = [] #fields or exclude variable must be specified
+        fields = ('title','body') #fields that must be rendered in the form, rest are hidden in a way
--- articles/models.py
+++ articles/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Article(models.Model):
     title = models.TextField(max_length=254)
     body = models.TextField()
-    likes = models.IntegerField()
+    likes = models.IntegerField(default=0)
     def __str__(self):
         return self.title
     def __unicode__(self):
